056592eee3509d815c986beeebe5bfaa829a20b2
Jeremy Tarling <jeremy.tarling@gmail.com>
2013-10-21 16:03:36 +0100

    migrated to creative works LDP API and added inferences for notable associations

--- app.rb
+++ app.rb
@@ -10,10 +10,19 @@ SSL = {
   :ssl_client_key => OpenSSL::PKey::RSA.new(File.read("./dependencies/cert.pem")),
 }  
 
-BASE_URL = "https://api.live.bbc.co.uk/ldp-core/creative-works-legacy?about="
+BASE_URL = "https://api.bbc.co.uk/ldp-core/creative-works?about="
 
-def getData
+BASE_URL_NAW = "https://api.bbc.co.uk/ldp-core/creative-works?about-tag-type=http%3A%2F%2Fwww.bbc.co.uk%2Fontologies%2Fnews%2FOrganisation&about-tag-predicate=http%3A%2F%2Fwww.bbc.co.uk%2Fontologies%2Fnews%2FnotablyAssociatedWith&about-tag-object="
+
+def getDataAbout
   @url = BASE_URL + params[:guid]
+  @data = RestClient::Resource.new(@url, SSL).get({:accept => "application/json-ld"}) 
+  @data_json = JSON.parse @data.body  
+  @results =  @data_json['results']
+end
+
+def getDataNaw
+  @url = BASE_URL_NAW + params[:guid]
   @data = RestClient::Resource.new(@url, SSL).get({:accept => "application/json-ld"}) 
   @data_json = JSON.parse @data.body  
   @results =  @data_json['results']
@@ -24,31 +33,36 @@ get '/' do
 end
 
 get '/storylines/:guid' do
-  getData
+  getDataAbout
   erb :storylines_article_list
 end
 
 get '/business_themes/:guid' do
-  getData
+  getDataAbout
   erb :business_themes_article_list
 end
 
+get '/business_themes_inferred/:guid' do
+  getDataNaw
+  erb :business_themes_inferred_article_list
+end
+
 get '/uk_companies/:guid' do
-  getData
+  getDataAbout
   erb :uk_companies_article_list
 end
 
 get '/international_companies/:guid' do
-  getData
+  getDataAbout
   erb :international_companies_article_list
 end
 
 get '/uk_institutions/:guid' do
-  getData
+  getDataAbout
   erb :uk_institutions_article_list
 end
 
 get '/international_institutions/:guid' do
-  getData
+  getDataAbout
   erb :international_institutions_article_list
 end
